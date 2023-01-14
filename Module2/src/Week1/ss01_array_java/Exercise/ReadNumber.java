d45877f616c073648adc5d9d06558cd1160d6071
giahuy16404 <phamhuy1642004@gmail.com>
2023-01-14 21:40:10 +0700

    src

--- Module2/src/Week1/ss01_array_java/Exercise/ReadNumber.java
+++ Module2/src/Week1/ss01_array_java/Exercise/ReadNumber.java
@@ -1,11 +1,8 @@
 package Week1.ss01_array_java.Exercise;
-
 import java.util.Scanner;
-
 public class ReadNumber {
-    static String n = "";
+    static String sumNumber = "";
     static int check = 0;
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         do {
@@ -19,218 +16,216 @@ public class ReadNumber {
                     if (number > 0 && number < 10) {
                         switch (number) {
                             case 1:
-                                n = "One";
+                                sumNumber = "One";
                                 break;
                             case 2:
-                                n = "Two";
+                                sumNumber = "Two";
                                 break;
                             case 3:
-                                n = "Three";
+                                sumNumber = "Three";
                                 break;
                             case 4:
-                                n = "Four";
+                                sumNumber = "Four";
                                 break;
                             case 5:
-                                n = "Five";
+                                sumNumber = "Five";
                                 break;
                             case 6:
-                                n = "Six";
+                                sumNumber = "Six";
                                 break;
                             case 7:
-                                n = "Seven";
+                                sumNumber = "Seven";
                                 break;
                             case 8:
-                                n = "Eight";
+                                sumNumber = "Eight";
                                 break;
                             case 9:
-                                n = "Nine";
+                                sumNumber = "Nine";
                                 break;
                         }
 
                     } else if (number >= 10 && number < 20) {
                         switch (number) {
                             case 10:
-                                n = "Ten";
+                                sumNumber = "Ten";
                                 break;
                             case 11:
-                                n = "Eleven";
+                                sumNumber = "Eleven";
                                 break;
                             case 12:
-                                n = "Twelve";
+                                sumNumber = "Twelve";
                                 break;
                             case 13:
-                                n = "thirteen";
+                                sumNumber = "thirteen";
                                 break;
                             case 14:
-                                n = "Fourteen";
+                                sumNumber = "Fourteen";
                                 break;
                             case 15:
-                                n = "fifteen";
+                                sumNumber = "fifteen";
                                 break;
                             case 16:
-                                n = "sixteen";
+                                sumNumber = "sixteen";
                                 break;
                             case 17:
-                                n = "seventeen";
+                                sumNumber = "seventeen";
                                 break;
                             case 18:
-                                n = "eighteen";
+                                sumNumber = "eighteen";
                                 break;
                             case 19:
-                                n = "nineteen";
+                                sumNumber = "nineteen";
                                 break;
                         }
                     } else if (number >= 20 && number < 100) {
                         switch (number / 10) {
                             case 2:
-                                n = "Twenty";
+                                sumNumber = "Twenty";
                                 break;
                             case 3:
-                                n = "Thirty";
+                                sumNumber = "Thirty";
                                 break;
                             case 4:
-                                n = "Forty";
+                                sumNumber = "Forty";
                                 break;
                             case 5:
-                                n = "Fifty";
+                                sumNumber = "Fifty";
                                 break;
                             case 6:
-                                n = "Sixty";
+                                sumNumber = "Sixty";
                                 break;
                             case 7:
-                                n = "seventy";
+                                sumNumber = "seventy";
                                 break;
                             case 8:
-                                n = "eighty";
+                                sumNumber = "eighty";
                                 break;
                             case 9:
-                                n = "ninety";
+                                sumNumber = "ninety";
                                 break;
                         }
                         switch (number % 10) {
                             case 1:
-                                n += "One";
+                                sumNumber += "One";
                                 break;
                             case 2:
-                                n += "Two";
+                                sumNumber += "Two";
                                 break;
                             case 3:
-                                n += "Three";
+                                sumNumber += "Three";
                                 break;
                             case 4:
-                                n += "Four";
+                                sumNumber += "Four";
                                 break;
                             case 5:
-                                n += "Five";
+                                sumNumber += "Five";
                                 break;
                             case 6:
-                                n += "Six";
+                                sumNumber += "Six";
                                 break;
                             case 7:
-                                n += "Seven";
+                                sumNumber += "Seven";
                                 break;
                             case 8:
-                                n += "Eight";
+                                sumNumber += "Eight";
                                 break;
                             case 9:
-                                n += "Nine";
+                                sumNumber += "Nine";
                                 break;
                         }
                     } else if (number >= 100 && number < 1000) {
                         switch (number / 100) {
                             case 1:
-                                n = "One hundred";
+                                sumNumber = "One hundred";
                                 break;
                             case 2:
-                                n = "Two hundred";
+                                sumNumber = "Two hundred";
                                 break;
                             case 3:
-                                n = "Three hundred";
+                                sumNumber = "Three hundred";
                                 break;
                             case 4:
-                                n = "Four hundred";
+                                sumNumber = "Four hundred";
                                 break;
                             case 5:
-                                n = "Five hundred";
+                                sumNumber = "Five hundred";
                                 break;
                             case 6:
-                                n = "Six hundred";
+                                sumNumber = "Six hundred";
                                 break;
                             case 7:
-                                n = "Seven hundred";
+                                sumNumber = "Seven hundred";
                                 break;
                             case 8:
-                                n = "Eight hundred";
+                                sumNumber = "Eight hundred";
                                 break;
                             case 9:
-                                n = "Nine hundred";
+                                sumNumber = "Nine hundred";
                                 break;
 
                         }
                         number = number % 100;
                         switch (number / 10) {
                             case 2:
-                                n += "Twenty";
+                                sumNumber += "Twenty";
                                 break;
                             case 3:
-                                n += "Thirty";
+                                sumNumber += "Thirty";
                                 break;
                             case 4:
-                                n += "Forty";
+                                sumNumber += "Forty";
                                 break;
                             case 5:
-                                n += "Fifty";
+                                sumNumber += "Fifty";
                                 break;
                             case 6:
-                                n += "Sixty";
+                                sumNumber += "Sixty";
                                 break;
                             case 7:
-                                n += "seventy";
+                                sumNumber += "seventy";
                                 break;
                             case 8:
-                                n += "eighty";
+                                sumNumber += "eighty";
                                 break;
                             case 9:
-                                n += "ninety";
+                                sumNumber += "ninety";
                                 break;
                         }
                         switch (number % 10) {
                             case 1:
-                                n += "One";
+                                sumNumber += "One";
                                 break;
                             case 2:
-                                n += "Two";
+                                sumNumber += "Two";
                                 break;
                             case 3:
-                                n += "Three";
+                                sumNumber += "Three";
                                 break;
                             case 4:
-                                n += "Four";
+                                sumNumber += "Four";
                                 break;
                             case 5:
-                                n += "Five";
+                                sumNumber += "Five";
                                 break;
                             case 6:
-                                n += "Six";
+                                sumNumber += "Six";
                                 break;
                             case 7:
-                                n += "Seven";
+                                sumNumber += "Seven";
                                 break;
                             case 8:
-                                n += "Eight";
+                                sumNumber += "Eight";
                                 break;
                             case 9:
-                                n += "Nine";
+                                sumNumber += "Nine";
                                 break;
                         }
 
                     }
-                    System.out.println(n);
+                    System.out.println(sumNumber);
                     break;
-
-
             }
 
-        } while (check <2);
+        } while (check < 2);
     }
 }
