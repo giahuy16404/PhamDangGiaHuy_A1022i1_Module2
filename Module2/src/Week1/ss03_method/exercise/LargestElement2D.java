22bd429240239f8fca47f5f777ceead3c740a2c5
giahuy16404 <phamhuy1642004@gmail.com>
2023-01-14 22:59:52 +0700

    src

--- Module2/src/Week1/ss03_method/exercise/LargestElement2D.java
+++ Module2/src/Week1/ss03_method/exercise/LargestElement2D.java
@@ -1,16 +1,18 @@
 package Week1.ss03_method.exercise;
+
 import java.util.Scanner;
+
 public class LargestElement2D {
     public static double maxNumber2D(double arr[][]) {
         double max = arr[0][0];
-        int  lineArr = 0;
+        int lineArr = 0;
         int columArr = 0;
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
                 if (max < arr[i][j]) {
                     max = arr[i][j];
-                    lineArr = j+1;
-                    columArr = j+1;
+                    lineArr = j + 1;
+                    columArr = j + 1;
                 }
             }
 
@@ -18,20 +20,20 @@ public class LargestElement2D {
         return max;
     }
 
-    public static String matrixCoordinates(double arr[][]){
+    public static String matrixCoordinates(double arr[][]) {
         double max = arr[0][0];
-        int  lineArr = 0;
+        int lineArr = 0;
         int columArr = 0;
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
                 if (max < arr[i][j]) {
                     max = arr[i][j];
-                    lineArr = j+1;
-                    columArr = j+1;
+                    lineArr = j + 1;
+                    columArr = j + 1;
                 }
             }
         }
-        return "\ttọa độ dòng :"+lineArr + "\tcột :" + columArr;
+        return "\ttọa độ dòng :" + lineArr + "\tcột :" + columArr;
     }
 
     public static void main(String[] args) {
@@ -49,6 +51,6 @@ public class LargestElement2D {
                 number2D[i][j] = sc.nextDouble();
             }
         }
-        System.out.println("Số lớn nhất trong mảng là:"+maxNumber2D(number2D) + matrixCoordinates(number2D));
+        System.out.println("Số lớn nhất trong mảng là:" + maxNumber2D(number2D) + matrixCoordinates(number2D));
     }
 }
