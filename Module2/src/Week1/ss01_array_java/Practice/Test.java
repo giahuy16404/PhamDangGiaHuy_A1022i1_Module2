597cdf2d03ccaa3650d5a92a7231bd01720ee2e4
giahuy16404 <phamhuy1642004@gmail.com>
2023-01-14 21:49:48 +0700

    src

---
+++ Module2/src/Week1/ss01_array_java/Practice/Test.java
@@ -0,0 +1,12 @@
+package Week1.ss01_array_java.Practice;
+
+public class Test {
+    public  static void SumTwo(int a , int b){
+        System.out.println(a+b);
+
+    }
+    public static void main(String[] args) {
+        SumTwo(2,4);
+
+    }
+}
