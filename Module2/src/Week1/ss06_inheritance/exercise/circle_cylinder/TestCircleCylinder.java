c30c7e77e856e11c575776870eab1314c0790fc9
giahuy16404 <phamhuy1642004@gmail.com>
2023-02-07 19:05:27 +0700

    src

--- Module2/src/Week1/ss06_inheritance/exercise/circle_cylinder/TestCircleCylinder.java
+++ Module2/src/Week1/ss06_inheritance/exercise/circle_cylinder/TestCircleCylinder.java
@@ -2,14 +2,12 @@ package Week1.ss06_inheritance.exercise.circle_cylinder;
 
 public class TestCircleCylinder {
     public static void main(String[] args) {
-        Circle circle = new Cylinder("Blue",12,5);
+        Circle circle = new Cylinder("Blue", 12, 5);
         System.out.println(circle.acreageCircle());
         System.out.println(circle);
 
-        Cylinder cylinder =  new Cylinder("blue",12,2);
+        Cylinder cylinder = new Cylinder("blue", 12, 2);
         System.out.println(cylinder.volumeCylinder());
         System.out.println(cylinder);
-
-
     }
 }
