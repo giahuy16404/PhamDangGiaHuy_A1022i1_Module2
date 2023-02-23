2600295b3af8d241692455de528a403463b67cbc
giahuy16404 <phamhuy1642004@gmail.com>
2023-02-23 14:22:55 +0700

    Src

--- Module2/src/homework/ss11_dsa_stack_queue/exercise/count_occurrences/PracticeTreeMap.java
+++ Module2/src/homework/ss11_dsa_stack_queue/exercise/count_occurrences/PracticeTreeMap.java
@@ -7,12 +7,15 @@ import java.util.TreeMap;
 public class PracticeTreeMap {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Map<Character, Integer> map = new TreeMap<>();
+
         System.out.println("Enter a string ");
         String arrString = sc.nextLine();
-        Map<Character, Integer> map = new TreeMap<>();
         arrString = arrString.toLowerCase();
+
         for (int i = 0; i < arrString.length(); i++) {
             char keyMap = arrString.charAt(i);
+
             if (map.containsKey(keyMap)) {
                 map.put(keyMap, map.get(keyMap) + 1);
             } else {
--- Module2/src/homework/ss11_dsa_stack_queue/exercise/palindrome/CheckPalindrome.java
+++ Module2/src/homework/ss11_dsa_stack_queue/exercise/palindrome/CheckPalindrome.java
@@ -7,16 +7,21 @@ public class CheckPalindrome {
         Queue<String> queue = new LinkedList<>();
         Stack<String> stack = new Stack<>();
         String palindromeString = "Able was I ere I saw Elba";
+
         char charPalindromeString;
+
         for (int i = 0; i < palindromeString.length(); i++) {
             charPalindromeString = palindromeString.charAt(i);
+
             stack.push(String.valueOf(charPalindromeString));
             queue.add(String.valueOf(charPalindromeString));
         }
         boolean checkPalindrome = true;
         while (!stack.isEmpty() && !queue.isEmpty()) {
+
             String stackElement = stack.pop();
             String queueElement = queue.remove();
+
             if (!stackElement.equalsIgnoreCase(queueElement)) {
                 checkPalindrome = false;
                 break;
