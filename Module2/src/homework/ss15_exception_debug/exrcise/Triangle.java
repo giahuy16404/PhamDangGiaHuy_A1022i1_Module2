0893f73b96cc56753427171ebc1d3ef94e1bce6b
giahuy16404 <phamhuy1642004@gmail.com>
2023-03-06 18:31:38 +0700

    src

--- Module2/src/homework/ss15_exception_debug/exrcise/Triangle.java
+++ Module2/src/homework/ss15_exception_debug/exrcise/Triangle.java
@@ -15,13 +15,13 @@ public class Triangle {
 
             System.out.println("Nhập vào số c");
             int c = sc.nextInt();
-            if (a < 0 && b < 0 && c < 0) {
+            if (a < 0 || b < 0 || c < 0) {
                 throw new IllegalTriangleException("Nhập một số lớn hơn 0");
             } else if (a + b < c || a + c < b || b + c < a) {
                 throw new IllegalTriangleException("nhập  tổng 2 cạnh không lớn hơn cạnh còn lại ");
             }
 
-        } catch (InputMismatchException | NumberFormatException e) {
+        } catch (InputMismatchException e) {
             System.out.println("Nhập số!");
         } catch (IllegalTriangleException e) {
             System.out.println(e.getMessage());
